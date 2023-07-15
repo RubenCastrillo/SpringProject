6eb8534d43ef573d2e645e1a4ee238b9aa96be73
bacru <bacruben@gmail.com>
2023-07-15 22:15:32 +0200

    Clase 27 Value

---
+++ spring-boot-web-1/src/main/java/com/bolsaideas/springboot/web/app/controllers/VariablesRutaController.java
@@ -0,0 +1,19 @@
+package com.bolsaideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class VariablesRutaController {
+
+	
+	@GetMapping("/string/{texto}")
+	public String variables(@PathVariable String texto,Model model) {
+		
+		return "variables/ver";
+	}
+}
